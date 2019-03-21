697c838cf4fdfb6e8d348d05c9f74256a393cb47
Doug Henderson <djhenderson@users.noreply.github.com>
2019-03-21 16:56:47 -0600

    my changes

--- main.go
+++ main.go
@@ -4,128 +4,200 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
+	"regexp"
+	"strings"
+)
 
-	"github.com/PuerkitoBio/goquery"
-	"github.com/reujab/wallpaper"
+const (
+	verbose     = false
+	logFileName = `D:/Users/Doug/Repositories/GitHub/bwall/bwall.log`
 )
 
 // getImageLink parses Bing and gets img url
 // https://www.devdungeon.com/content/web-scraping-go
-func getImageURL() string {
-	domain, url := "https://www.bing.com/", ""
+func getImageURL(domain string) string {
 	// Make HTTP GET request
+	if verbose {
+		fmt.Println("Domain          :", domain)
+	}
 	response, err := http.Get(domain)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err.Error())
 	}
 	defer response.Body.Close()
 
-	// Turn HTML string to document
-	document, err := goquery.NewDocumentFromReader(response.Body)
-	if err != nil {
-		log.Fatal("Error loading HTTP response body.\n", err)
+	if verbose {
+		fmt.Println("Status          :", response.Status)
+		fmt.Println("StatusCode      :", response.StatusCode)
+		fmt.Println("Proto           :", response.Proto)
+		fmt.Println("ProtoMajor      :", response.ProtoMajor)
+		fmt.Println("ProtoMinor      :", response.ProtoMinor)
+		fmt.Println("Header          :")
+		for k, v := range response.Header {
+			fmt.Println("Header          :", k, ":", v)
+		}
+		fmt.Println()
+		fmt.Println("ContentLength   :", response.ContentLength)
+		fmt.Println("TransferEncoding:", response.TransferEncoding)
+		fmt.Println("Uncompressed    :", response.Uncompressed)
+		//fmt.Println("TLS             :", response.TLS) // too long
 	}
 
-	// Find background url
-	document.Find("link#bgLink").Each(func(index int, element *goquery.Selection) {
-		imgSrc, exists := element.Attr("href")
-		if exists {
-			url = domain + imgSrc
+	body_bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	if len(body_bytes) <= 0 {
+		fmt.Println("body_bytes - len, cap:", len(body_bytes), cap(body_bytes))
+		log.Fatalln("Empty body")
+	}
+
+	expr := `<link(( id="(?P<id>bgLink)")|( rel="(?P<rel>[^"]+)")|( href="(?P<href>[^"]+)")|( as="(?P<as>[^"]+)"))+ />`
+	re, err := regexp.Compile(expr)
+
+	subexpNames := re.SubexpNames()
+
+	var page string
+	if verbose {
+		fmt.Printf("SubexpNames: %q\n", subexpNames)
+
+		fmt.Println("Match:", re.Match(body_bytes))
+		fmt.Println()
+		fmt.Println("FindSubmatch:")
+	}
+	for i, v := range re.FindSubmatch(body_bytes) {
+		if len(subexpNames[i]) > 0 {
+			if verbose {
+				fmt.Printf("%2d %-4s: %s\n", i, subexpNames[i], string(v))
+			}
+			if subexpNames[i] == "href" {
+				page = string(v)
+			}
 		}
-	})
+	}
+
+	url := domain + page
+	if verbose {
+		fmt.Println()
+		fmt.Println("url:", url)
+	}
 
 	return url
 }
 
 // exists returns whether the given file or directory exists
 // https://stackoverflow.com/a/10510783
-func exists(path string) (bool, error) {
+func exists(path string) (result bool) {
 	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
+	if err == nil || os.IsExist(err) {
+		result = true
+	} else if os.IsNotExist(err) {
+		result = false
+	} else {
+		log.Fatalln(err.Error())
 	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return true, err
+	return result
 }
 
 // downloadImg saves image to ./.data directory
 // and returns file path
 // https://stackoverflow.com/questions/22417283/save-an-image-from-url-to-file
 func downloadImg(url string) string {
-	dir := "./.data/"
-	exists, err := exists(dir)
+	dir := "D:/Users/Doug/Pictures/Wallpaper/Bing"
 
-	if err != nil {
-		log.Fatal("Error finding directory:\n", err)
+	if verbose {
+		fmt.Println("url:", url)
+		fmt.Println("dir:", dir)
 	}
 
-	if !exists {
-		err = os.Mkdir(dir, 0777)
-
+	found := exists(dir)
+	if !found {
+		err := os.MkdirAll(dir, 0766)
 		if err != nil {
-			log.Fatal("Couldn't Create Directory\n", err)
+			log.Println("dir:", dir)
+			log.Fatalln("Couldn't Create Directory:", err)
 		}
 	}
 
 	// get image
 	response, err := http.Get(url)
 	if err != nil {
-		log.Fatal("Couln't Download Image\n", err)
+		log.Println("url:", url)
+		log.Fatalln("Couldn't Download Image:", err)
 	}
 	defer response.Body.Close()
 
-	// create and open file
-	i := len(url) - 1
-	for i >= 0 && url[i] != '.' {
-		i--
+	lastSlash := strings.LastIndex(url, "/")
+	fileName := url[lastSlash+1:]
+	goodFilename := false
+	if strings.HasSuffix(fileName, ".jpg") {
+		// log.Println("HasSuffix of '.jpg'")
+		goodFilename = true
+	} else if strings.HasPrefix(fileName, "th?id=") && strings.Contains(fileName, ".jpg&amp;") {
+		fileName = fileName[6:]
+		// log.Println("fileName:", fileName)
+		endX := strings.Index(fileName, ".jpg&amp;")
+		fileName = fileName[:endX+4]
+		// log.Println("fileName:", fileName)
+		goodFilename = true
 	}
-	extension := url[i:]
-	filePath := dir + strconv.FormatInt(time.Now().Unix(), 10) + extension
+	if !goodFilename {
+		log.Fatalln("Cannot guess local filename.")
+	}
+	filePath := dir + "/" + fileName
 
+	// create and/or (re)open file
 	file, err := os.Create(filePath)
 	if err != nil {
-		log.Fatal("Couldn't Create File\n", err)
+		log.Println("filePath:", filePath)
+		log.Fatalln("Couldn't Create and (re)open File:", err)
 	}
 	defer file.Close()
 
 	// Copy to file
 	_, err = io.Copy(file, response.Body)
 	if err != nil {
-		log.Fatal("Couldn't Save Image\n", err)
+		log.Fatalln("Couldn't Save Image:", err)
 	}
 
-	// Get current directory
-	// https://gist.github.com/arxdsilva/4f73d6b89c9eac93d4ac887521121120
-	dir, err = os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return dir + filePath[1:]
+	return filePath
 }
 
-func setImageAsWallpaper() {
-	url := getImageURL()
-	file := downloadImg(url)
-	wallpaper.SetFromFile(file)
-	fmt.Println(file)
-}
+//func setImageAsWallpaper() {
+//url := getImageURL()
+//file := downloadImg(url)
+//wallpaper.SetFromFile(file)
+//fmt.Println(file)
+//}
 
-// https://gist.github.com/ryanfitz/4191392
+//// https://gist.github.com/ryanfitz/4191392
+//func routine() {
+//setImageAsWallpaper()
+//for range time.Tick(24 * time.Hour) {
+//setImageAsWallpaper()
+//}
+//}
+
 func routine() {
-	setImageAsWallpaper()
-	for range time.Tick(24 * time.Hour) {
-		setImageAsWallpaper()
-	}
+	domain := "https://www.bing.com"
+	url := getImageURL(domain)
+	log.Println("Image found at:", url)
+	filePath := downloadImg(url)
+	log.Println("Image saved to:", filePath)
 }
 
 func main() {
+	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+	log.Println("")
+
 	routine()
 }
